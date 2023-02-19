f21263941659d880135f99c4fa90e3e0aef1f19d
striter <letsbattle@foxmail.com>
2023-02-20 01:58:55 +0800

    Update

--- source/lua/shine/extensions/enforceteamsizes.lua
+++ source/lua/shine/extensions/enforceteamsizes.lua
@@ -11,8 +11,10 @@ Plugin.PrintName = "Enforced Team Size"
  - 3: Spec
  ]]
 Plugin.DefaultConfig = {
-	Team = { 14 , 14},
-	SkillLimit = -1,
+	Team = { 14 , 14 },
+	IncreaseByForceJoins = true,
+	SkillLimitMin = -1,
+	SkillLimitMax = -1,
 	MessageNameColor = {0, 255, 0 },
 }
 Plugin.CheckConfig = true
@@ -22,10 +24,21 @@ Plugin.EnabledGamemodes = {
 	[ "ns2" ] = true,
 	[ "NS2.0" ] = true,
 	[ 'siege+++' ] = true,
-	[ 'GunGame' ] =true,
+	[ 'GunGame' ] = true,
+	[ 'Combat' ] = true,
 }
 
+do
+	local Validator = Shine.Validator()
+	Validator:AddFieldRule( "Team",  Validator.IsType( "table", {} ))
+	Validator:AddFieldRule( "IncreaseByForceJoins",  Validator.IsType( "boolean", true ))
+	Validator:AddFieldRule( "SkillLimitMin",  Validator.IsType( "number", -1 ))
+	Validator:AddFieldRule( "SkillLimitMax",  Validator.IsType( "number", -1 ))
+	Validator:AddFieldRule( "MessageNameColor",  Validator.IsType( "table", {0,255,0} ))
+	Plugin.ConfigValidator = Validator
+end
 local priorColorTable = { 237, 187, 153  }
+
 
 function Plugin:Initialise()
 	self.Enabled = true
@@ -49,21 +62,31 @@ local TeamNames = { "陆战队","卡拉异形" }
 function Plugin:JoinTeam( Gamerules, Player, NewTeam, _, ShineForce )
 	if ShineForce or NewTeam == kTeamReadyRoom or NewTeam == kSpectatorIndex then return end
 	if self.Config.IgnoreBots and Player:GetIsVirtual() then return end
-
 	
 	local skill = Player:GetPlayerSkill()
 	
 	local available
 	
-	if self.Config.SkillLimit ~= -1 and skill > self.Config.SkillLimit  then
-		self:Notify(Player, string.format("您的分数(%s)以超过服务器上限(%s),请继续观战或加入其他服务器.", skill, self.Config.SkillLimit),self.Config.MessageNameColor,nil)
+	local skillLimited = false
+	skillLimited = skillLimited or (skill < self.Config.SkillLimitMin)
+	skillLimited = skillLimited or (self.Config.SkillLimitMax ~= -1 and skill > self.Config.SkillLimitMax)
+	if skillLimited then
+		self:Notify(Player, string.format("您的分数(%s)不在服务器限制内(%s-%s),请继续观战或加入其他服务器.",
+				skill, self.Config.SkillLimitMin,self.Config.SkillLimitMax == -1 and "∞" or self.Config.SkillLimitMax),
+					self.Config.MessageNameColor,nil)
 		available = false
 	end
 	
 	--Check if team is above MaxPlayers
 	local playerLimit = self.Config.Team[NewTeam]
+	if self.Config.IncreaseByForceJoins then
+		local maxPlayers = math.max(self:GetNumPlayers(Gamerules:GetTeam(kTeam1Index)),self:GetNumPlayers(Gamerules:GetTeam(kTeam2Index)))
+		playerLimit = math.max(playerLimit,maxPlayers)
+	end
+	
 	if self:GetNumPlayers(Gamerules:GetTeam(NewTeam)) >= playerLimit then
-		self:Notify(Player,string.format( "[%s]人数已满(>=%s),请继续观战,等待空位或加入有空位的服务器.", TeamNames[NewTeam] ,playerLimit),self.Config.MessageNameColor,nil)
+		self:Notify(Player,string.format( "[%s]人数已满(>=%s),请继续观战,等待空位或加入有空位的服务器.", TeamNames[NewTeam] ,playerLimit),
+				self.Config.MessageNameColor,nil)
 		available = false
 	end
 
@@ -79,13 +102,16 @@ function Plugin:JoinTeam( Gamerules, Player, NewTeam, _, ShineForce )
 	return available
 end
 
+local function RestrictionDisplay(_client)
+	local skillLimitMin = self.Config.SkillLimitMin
+	local skillLimitMax = self.Config.SkillLimitMax == -1 and "∞" or tostring(self.Config.SkillLimitMax)
+
+	self:Notify(_client,string.format("当前加入限制:陆战队:%s,卡拉异形:%s 分数限制:(%s至%s)", self.Config.Team[1], self.Config.Team[2],skillLimitMin,skillLimitMax),self.Config.MessageNameColor,nil)
+end
+
 function Plugin:CreateCommands()
 
-	local function RestrictionDisplay(_client)
-		local skillLimit = self.Config.SkillLimit == -1 and "无限制" or tostring(self.Config.SkillLimit)
-		self:Notify(_client,string.format("当前加入限制为:[陆战队]:%s,[卡拉异形]:%s,[最高分数](%s)", self.Config.Team[1], self.Config.Team[2],skillLimit),self.Config.MessageNameColor,nil)
-	end
-	local showRestriction = self:BindCommand( "sh_restriction", "restriction", RestrictionDisplay)
+	local showRestriction = self:BindCommand( "sh_restriction", "restriction", RestrictionDisplay , true) 
 	showRestriction:Help( "示例: !restriction 传回当前的队伍限制" )
 	
 	local function NofityAll()
@@ -106,22 +132,28 @@ function Plugin:CreateCommands()
 	teamsizeCommand:AddParam{ Type = "number", Round = true, Min = 0, Max = 28, Default = 10 }
 	teamsizeCommand:AddParam{ Type = "number", Round = true, Min = 0, Max = 28, Default = 10 }
 	teamsizeCommand:AddParam{ Type = "boolean", Default = false, Help = "true = 保存设置", Optional = true  }
-	teamsizeCommand:Help( "示例: !restriction_size 14 12. 将服务器的队伍人数上限设置为,队伍一(陆战队):14人,队伍二(卡拉):12人" )
+	teamsizeCommand:Help( "示例: !restriction_size 14 12 true. 将服务器的队伍人数上限设置为,队伍一(陆战队):14人,队伍二(卡拉):12人 并保存" )
 
-	local function SetSkillLimit(_client, _skillLimit,_save)
-		self.Config.SkillLimit = _skillLimit
+	local function SetSkillLimit(_client, _min,_max,_save)
+		self.Config.SkillLimitMin = _min
+		self.Config.SkillLimitMax = _max
+
 		if _save then
 			self:SaveConfig()
 		end
 		NofityAll()
 	end
 	local skillCommand = self:BindCommand( "sh_restriction_skill", "restriction_skill", SetSkillLimit)
-	skillCommand:AddParam{ Type = "number", Round = true, Min = -1, Max = 99999, Default = -1 }
+	skillCommand:AddParam{ Type = "number", Round = true, Min = -1, Max = 4000, Default = -1 }
+	skillCommand:AddParam{ Type = "number", Round = true, Min = -1, Max = 4000, Default = -1 }
 	skillCommand:AddParam{ Type = "boolean", Default = false, Help = "true = 保存设置", Optional = true  }
-	skillCommand:Help( "示例: !restriction_skill 1000. 将服务器的分数上限设置为,最大分数(-1为全部通过):1000" )
+	skillCommand:Help( "示例: !restriction_skill 1000 -1 true.将服务器的入场分数设置为,[1000-∞],并且保存,-1代表无限制" )
 end
 
 
+function Plugin:ClientConfirmConnect( Client )
+	RestrictionDisplay(Client)
+end
 --Restrict teams also at voterandom
 function Plugin:PreShuffleOptimiseTeams ( TeamMembers )
 	local  Gamerules = GetGamerules()
