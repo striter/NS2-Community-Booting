8850ec1290cd8534d69f972c310b34b53f8d28a3
striter <letsbattle@foxmail.com>
2023-05-08 15:16:26 +0800

    Update ScoringMixin.lua

--- source/lua/CNCommunityRank/ScoringMixin.lua
+++ source/lua/CNCommunityRank/ScoringMixin.lua
@@ -24,52 +24,22 @@ function ScoringMixin:GetCommanderSkill()
 end
 
 if Server then
-
+    local baseCopyPlayerDataFrom = ScoringMixin.CopyPlayerDataFrom
     function ScoringMixin:CopyPlayerDataFrom(player)
-    
-        self.scoreGainedCurrentLife = player.scoreGainedCurrentLife    
-        self.score = player.score or 0
-        self.kills = player.kills or 0
-        self.assistkills = player.assistkills or 0
-        self.deaths = player.deaths or 0
-        self.playTime = player.playTime or 0
-        self.commanderTime = player.commanderTime or 0
-        self.marineTime = player.marineTime or 0
-        self.alienTime = player.alienTime or 0
+        baseCopyPlayerDataFrom(self,player)
         
-        self.weightedEntranceTimes = player.weightedEntranceTimes
-        self.weightedExitTimes = player.weightedExitTimes
-        
-        self.weightedCommanderTimes = player.weightedCommanderTimes
-
-        self.teamAtEntrance = player.teamAtEntrance
-        
-        self.totalKills = player.totalKills
-        self.totalAssists = player.totalAssists
-        self.totalDeaths = player.totalDeaths
-        self.skill = player.skill
-        self.skillOffset = player.skillOffset
-        self.commSkill = player.commSkill
-        self.commSkillOffset = player.commSkillOffset
-        self.adagradSum = player.adagradSum
-        self.commAdagradSum = player.commAdagradSum
-        self.skillTier = player.skillTier
-        self.totalScore = player.totalScore
-        self.totalPlayTime = player.totalPlayTime
-        self.playerLevel = player.playerLevel
-        self.totalXP = player.totalXP
-
-----------
         self.rankDelta = player.rankDelta
         self.rankCommDelta = player.rankCommDelta
         self.fakeBot = player.fakeBot
         self.emblem = player.emblem
+        
         self.prewarmTier = player.prewarmTier
         self.prewarmTime = player.prewarmTime
+        
         self.group = player.group
+        
         self.queueIndex = player.queueIndex
         self.reservedQueueIndex = player.reservedQueueIndex
----------
     end
 
     function ScoringMixin:SetPlayerExtraData(dataTable)
