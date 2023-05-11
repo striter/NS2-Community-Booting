7bb246f2da887dc5048bbbd4090306ef3b3edb8f
striter <letsbattle@foxmail.com>
2023-05-11 16:24:01 +0800

    Delete GUIMenuCallingCardCustomizer.lua

--- source/lua/CNCommunityRank/GUIMenuCallingCardCustomizer.lua
+++
@@ -1,9 +0,0 @@
-local baseOnCallingCardSelect = GUIMenuCallingCardCustomizer.OnCallingCardSelected
-function GUIMenuCallingCardCustomizer:OnCallingCardSelected(callingCardObj)
-    baseOnCallingCardSelect(self,callingCardObj)
-    if not CNPersistent then return end
-    local cardId = callingCardObj:GetCardID()
-    CNPersistent.callingCardID = cardId
-    CNPersistentSave()
-    Shared.Message("Save" .. tostring(cardId))
-end
