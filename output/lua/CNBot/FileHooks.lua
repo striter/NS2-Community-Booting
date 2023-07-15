d0a2df3b7c3bf7196075a8b5542aecfab4a2fd45
striter <letsbattle@foxmail.com>
2023-07-15 09:55:33 +0800

    Remed calling

--- output/lua/CNBot/FileHooks.lua
+++ output/lua/CNBot/FileHooks.lua
@@ -1,3 +1,2 @@
 ﻿ModLoader.SetupFileHook("lua/bots/BotAim.lua", "lua/CNBot/BotAim.lua", "post")
 ModLoader.SetupFileHook("lua/bots/PlayerBot_Server.lua", "lua/CNBot/PlayerBot_Server.lua", "post")
-ModLoader.SetupFileHook("lua/bots/MarineBrain.lua", "lua/CNBot/MarineBrain.lua", "post")
